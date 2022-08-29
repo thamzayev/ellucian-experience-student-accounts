8af24ac84e9ed38d35003ac1a3b35875322cc01c
thamzayev <thamzayev@ada.edu.az>
2022-08-29 15:59:25 +0400

    Initial commit

---
+++ extension.js
@@ -0,0 +1,68 @@
+module.exports = {
+    "name": "Student Account",
+    "publisher": "ADA University",
+    "cards": [{
+        "type": "StudentAccountCard",
+        "source": "./src/cards/StudentAccountCard",
+        "title": "Student Account",
+        "displayCardType": "Student Account Information",
+        "description": "This Card shows students' account balance",
+        "configuration": {
+          "client": [{
+            "key": "buttonUrl",
+            "label": "Account Balance button redirect URL",
+            "type":"url",
+            "required":true
+          },
+          {
+            "key": "currency",
+            "label": "Currency to show balances",
+            "type":"url",
+            "required":true
+          }]
+        },
+        "queries": {
+            "list-charges": [{
+                "resourceVersions": {
+                    "studentCharges": { min: 16 },
+                    "persons": { min: 12 }
+                },
+                "query": `query getStudentCharges($personId:ID)
+                {
+                  studentCharges16(
+                    filter : {
+                        student12:{id:{EQ:$personId}}
+                      })
+                  {
+                    edges {
+                      node {
+                        chargedAmount {
+                          amount {
+                            value
+                          }
+                        }
+                      }
+                    }
+                  }
+                }`
+            }],
+            "list-payments": [{
+                "resourceVersions": {
+                    "studentPayments": { min: 16 },
+                    "persons": { min: 12 }
+                },
+                "query":`query getStudentPayments($personId:ID){
+                    studentPayments16(filter: {student12:{id:{EQ:$personId}}}) {
+                        edges {
+                            node {
+                                amount{
+                                    value
+                                }
+                            }
+                        }
+                    }
+                }`
+            }]
+        }
+    }]
+}
---
+++ src/cards/StudentAccountCard.jsx
@@ -0,0 +1,124 @@
+import { withStyles } from "@ellucian/react-design-system/core/styles";
+import { spacing40 } from "@ellucian/react-design-system/core/styles/tokens";
+import {
+  Typography,
+  Divider,
+  Button
+} from "@ellucian/react-design-system/core";
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+
+const styles = () => ({
+  card: {
+    marginTop: 0,
+    marginRight: spacing40,
+    marginBottom: 0,
+    marginLeft: spacing40
+  },
+  label: {
+    float: "left"
+  },
+  button: {
+    bottom: spacing40,
+    right: spacing40,
+    position: "absolute"
+  }
+});
+
+const StudentAccountCard = (props) => {
+  const {
+    classes,
+    cardControl: { setLoadingStatus, setErrorMessage },
+    data: { getEthosQuery }
+  } = props;
+  const [totalCharges, setTotalCharges] = useState(0);
+  const [totalPayments, setTotalPayments] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      setLoadingStatus(true);
+
+      try {
+        const chargesData = await getEthosQuery({ queryId: "list-charges" });
+        const paymentsData = await getEthosQuery({ queryId: "list-payments" });
+        const {
+          data: {
+            studentCharges16: { edges: chargeEdges }
+          }
+        } = chargesData;
+        console.log(chargeEdges);
+        const {
+          data: {
+            studentPayments16: { edges: paymentEdges }
+          }
+        } = paymentsData;
+        const totalCharges =
+          Math.round(
+            100 *
+              chargeEdges.reduce(
+                (result, item) => result + item.node.chargedAmount.amount.value,
+                0
+              )
+          ) / 100;
+        const totalPayments =
+          Math.round(
+            100 *
+              paymentEdges.reduce(
+                (result, item) => result + item.node.amount.value,
+                0
+              )
+          ) / 100;
+
+        setTotalCharges(() => totalCharges);
+        setTotalPayments(() => totalPayments);
+        setTotal(() => totalPayments - totalCharges);
+        setLoadingStatus(false);
+      } catch (error) {
+        console.log("ethosQuery failed", error);
+        setErrorMessage({
+          headerMessage: "Fetch failed",
+          textMessage:
+            "Fetching data from student account has failed. Try again later.",
+          iconName: "error",
+          iconColor: "#D42828"
+        });
+      }
+    })();
+  }, []);
+
+  return (
+    <div className={classes.card}>
+      <Typography variant="h3" align="right">
+        <span className={classes.label}>Total charges:</span> {totalCharges}
+        {props.cardInfo.configuration.currency}
+      </Typography>
+      <Typography variant="h3" align="right">
+        <span className={classes.label}>Total payments:</span> {totalPayments}
+        {props.cardInfo.configuration.currency}
+      </Typography>
+      <Divider />
+      <Typography variant="h2" align="right">
+        <span className={classes.label}>Total balance:</span> {total}
+        {props.cardInfo.configuration.currency}
+      </Typography>
+      <Button
+        className={classes.button}
+        onClick={() => {
+          window.open(props.cardInfo.configuration.buttonUrl, "_blank");
+        }}
+      >
+        Account Details
+      </Button>
+    </div>
+  );
+};
+
+StudentAccountCard.propTypes = {
+  cardControl: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
+  cardInfo: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(StudentAccountCard);
